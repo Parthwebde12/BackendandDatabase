a93a133a14fb35f0c4cfa5e3efdef1c2110c6fa2
Parthwebde12 <parthwakodikar4@gmail.com>
2026-04-14 09:45:27 +0530

    done

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 
 app.use(express.json());
-app.use(express.urlencoded({urlencoded:true}));
-app.set('view engine','ejs');
+app.use(express.urlencoded({urlencoded:true})); //we will be handle form data 
+app.set('view engine','ejs'); // render kar sakte ejs pages ko
+app.use(express.static(path.join(__dirname,'public'))); //public static files use kra payenge
 
 app.get("/",function(req , res){
-    res.render("index");
+    res.render("index");  //
 })
 
 
